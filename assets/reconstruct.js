b8d27a9fa7ae88c085fbda8a385177a291965d6e
AZsSPC <Vjycnh13!az>
2022-09-11 23:54:37 +0300

    mega fix

--- assets/reconstruct.js
+++
@@ -1,8 +0,0 @@
-let width = window.screen.availWidth;
-const project = document.getElementsByClassName("skill");
-
-if (width <= 1000) {
-    for (let i of project)
-    i.classList.replace("skill", "skill_mobile");
-    
-};
